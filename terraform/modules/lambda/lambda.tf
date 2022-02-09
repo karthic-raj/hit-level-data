79a315053177df1f9bbfc771d299517fed835e34
karthicmech04@gmail.com <karthicmech04@gmail.com>
2022-02-09 00:59:35 -0600

    Lambda Updated

--- terraform/modules/lambda/lambda.tf
+++ terraform/modules/lambda/lambda.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      	= "hit_data.zip"
   role          	= var.lambdaRole
   handler       	= "hit_data.lambda_handler"
-  runtime 			= "python3.8"
+  runtime 			= "python3.7"
   source_code_hash 	= filebase64sha512("hit_data.zip")
   layers			= [aws_lambda_layer_version.lambda_layer.arn]
 }
