9c31d9e2671dc0c806f51e4ef7027188b3588409
karthicmech04@gmail.com <karthicmech04@gmail.com>
2022-02-09 00:20:49 -0600

    Layer Added

--- terraform/modules/lambda/lambda.tf
+++ terraform/modules/lambda/lambda.tf
@@ -1,3 +1,11 @@
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = "./src/Pandas_Package.zip"
+  layer_name = "panda_pacakages"
+
+  compatible_runtimes = ["python3.7"]
+}
+
+
 resource "aws_lambda_function" "test_lambda" {
   function_name 	= var.lambdaName
   filename      	= "hit_data.zip"
@@ -5,4 +13,5 @@ resource "aws_lambda_function" "test_lambda" {
   handler       	= "hit_data.lambda_handler"
   runtime 			= "python3.8"
   source_code_hash 	= filebase64sha512("hit_data.zip")
+  layers			= [aws_lambda_layer_version.lambda_layer.arn]
 }
--- terraform/src/hit_data.py
+++ terraform/src/hit_data.py
@@ -1,9 +1,17 @@
 import boto3
 import json
 
+s3      = boto3.client('s3')
+bucket  = "aws-us-east-1-test-bucket"
+key     = "hitdata/data.tsv"
+    
 def lambda_handler(event, context):
-    # TODO implement
-    print("test")
+    
+    #Read Data from S3
+    response = s3.get_object(Bucket=bucket, Key=key)
+    emailcontent = response['Body'].read().decode('utf-8')
+    print(emailcontent)
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
